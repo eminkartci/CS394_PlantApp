29b4f8f9a413144c6809d4c34e4f399081f47f32
eminkartci <emin.kartci@ozu.edu.tr>
2021-12-19 16:42:52 +0300

    Plant entitiy is created

---
+++ app/src/main/java/com/example/cs394_hw_plantapp/data/Plant.kt
@@ -0,0 +1,15 @@
+package com.example.cs394_hw_plantapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.net.URI
+
+@Entity(tableName = "plants")
+data class Plant(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int,
+    val name:String,
+    val family_name:String,
+    val scientific_name:String,
+    val image:URI
+)
