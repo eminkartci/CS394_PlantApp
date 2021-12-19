31fa9f6805d76f0f11ccc54e7d4b514727676b8c
eminkartci <emin.kartci@ozu.edu.tr>
2021-12-19 18:57:24 +0300

    Database structure is completed

--- app/src/main/java/com/example/cs394_hw_plantapp/data/PlantViewModel.kt
+++ app/src/main/java/com/example/cs394_hw_plantapp/data/PlantViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.cs394_hw_plantapp.data
 
-class PlantViewModel() {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class PlantViewModel(application:Application): AndroidViewModel {
+
+    private val readAllData: LiveData<List<Plant>>
+    private val repository: PlantRepository
+    init {
+        val plantDao = PlantDatabase.getDatabase(application).plantDao()
+        repository = PlantRepository(plantDao)
+        readAllData = repository.readAllData
+
+    }
+
+    fun addPlant(plant:Plant){
+        viewModelScope.launch (Dispatchers.IO){
+            repository.addPlant(plant)
+        }
+    }
 }
